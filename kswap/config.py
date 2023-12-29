357efcf0274de4ed560c4b3ff36bdbc7f734dc1c
Phil Holloway <54064261+P-1884@users.noreply.github.com>
2023-12-29 12:20:53 +0000

    Minor bug fixes

--- kswap/config.py
+++ kswap/config.py
@@ -20,13 +20,13 @@ class Config(object):
 
 #File Paths: 
     #File-path for list of subjects which have been retired:
-    self.retired_items_path= '../kswap/retired_items_list.txt #This should start with 'Column 1' as its first line.
+    self.retired_items_path= '../kswap/retired_items_list.txt' #This should start with 'Column 1' as its first line.
     #File-path for list of subjects which have been retired because they reached the classification number limit:
     self.Nclass_retirement_path = '../kswap/Nclass_retirement_list.txt'  #This should start with 'Column 1' as its first line.
     #File-path for list of subjects which have been retired because they reached the lower or upper score thresholds:
     self.Nthres_retirement_path = '../kswap/Nthres_retirement_list.txt' #This should start with 'Column 1' as its first line.
     #File-path for where the gold-subject csv is stored:
-    self.golds_path = XXX.csv
+    self.golds_path = './data/golds_file.csv' #This is currently a placeholder file.
     #If importing classifications from a csv file, this should be the path to that file, otherwise None:
     self.classification_path = None
     #File-path to the csv file for identifying the hard sims (if any), otherwise None:
@@ -38,10 +38,10 @@ class Config(object):
     
     #File-path for error-flags. The file should originally be a single line of [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     #These numbers are updated (+1) as swap runs, each time abnormal behaviour is detected. 
-    self.keyerror_list_path = XXX.txt
+    self.keyerror_list_path = '../kswap/KeyError_list.txt'
     #File-path for number of classifications retrieved. The file should originally be a single line of [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     #These numbers are updated (+1) as swap runs, indicating the number of classifications retrieved from the AWS queue (0 to 10 in the list). 
-    self.aws_list_path = XXX.txt
+    self.aws_list_path = '../kswap/AWS_list.txt'
     #This is the number of batches of 10 classifications retrieved from the queue at a time, before they're sent for processing by SWAP.
     self.number_of_batches_of_10 = 20 
 #IMPORTANT NOTE: There is an additional config file within caesar_extractor (opt/anaconda3/lib/python3.8/site-packages/caesar_external/data/test_config2.json) which needs to be configured to the correct workflow etc.
